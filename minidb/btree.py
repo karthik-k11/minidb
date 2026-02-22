700725f9f32e211459cb3bcc03ead8503a4e22b9
Karthik <karthikk1162@gmail.com>
2026-02-22 21:48:15 +0530

    Indentation cmodified for split root function

--- minidb/btree.py
+++ minidb/btree.py
@@ -1,11 +1,13 @@
 ORDER = 4
 
+
 class BTreeNode:
     def __init__(self, leaf=True):
         self.leaf = leaf
         self.keys = []
         self.values = []
         self.children = []
+
 
 class BTree:
     def __init__(self):
@@ -24,60 +26,61 @@ class BTree:
 
         return self.search(node.children[i], key)
 
-def split_child(self, parent, index):
-    full_child = parent.children[index]
-    new_child = BTreeNode(leaf=full_child.leaf)
-    mid = ORDER // 2
+    def split_child(self, parent, index):
+        full_child = parent.children[index]
+        new_child = BTreeNode(leaf=full_child.leaf)
 
-    parent.keys.insert(index, full_child.keys[mid])
-    parent.values.insert(index, full_child.values[mid])
-    parent.children.insert(index + 1, new_child)
+        mid = ORDER // 2
 
-    new_child.keys = full_child.keys[mid + 1:]
-    new_child.values = full_child.values[mid + 1:]
+        parent.keys.insert(index, full_child.keys[mid])
+        parent.values.insert(index, full_child.values[mid])
+        parent.children.insert(index + 1, new_child)
 
-    full_child.keys = full_child.keys[:mid]
-    full_child.values = full_child.values[:mid]
+        new_child.keys = full_child.keys[mid + 1:]
+        new_child.values = full_child.values[mid + 1:]
 
-    if not full_child.leaf:
-        new_child.children = full_child.children[mid + 1:]
-        full_child.children = full_child.children[:mid + 1]
+        full_child.keys = full_child.keys[:mid]
+        full_child.values = full_child.values[:mid]
 
-def insert_non_full(self, node, key, value):
-    i = len(node.keys) - 1
+        if not full_child.leaf:
+            new_child.children = full_child.children[mid + 1:]
+            full_child.children = full_child.children[:mid + 1]
 
-    if node.leaf:
-        node.keys.append(None)
-        node.values.append(None)
+    def insert_non_full(self, node, key, value):
+        i = len(node.keys) - 1
 
-        while i >= 0 and key < node.keys[i]:
-            node.keys[i + 1] = node.keys[i]
-            node.values[i + 1] = node.values[i]
-            i -= 1
+        if node.leaf:
+            node.keys.append(None)
+            node.values.append(None)
 
-        node.keys[i + 1] = key
-        node.values[i + 1] = value
-    else:
-        while i >= 0 and key < node.keys[i]:
-            i -= 1
-        i += 1
+            while i >= 0 and key < node.keys[i]:
+                node.keys[i + 1] = node.keys[i]
+                node.values[i + 1] = node.values[i]
+                i -= 1
 
-        if len(node.children[i].keys) == ORDER - 1:
-            self.split_child(node, i)
+            node.keys[i + 1] = key
+            node.values[i + 1] = value
+        else:
+            while i >= 0 and key < node.keys[i]:
+                i -= 1
+            i += 1
 
-            if key > node.keys[i]:
-                i += 1
+            if len(node.children[i].keys) == ORDER - 1:
+                self.split_child(node, i)
 
-        self.insert_non_full(node.children[i], key, value)
+                if key > node.keys[i]:
+                    i += 1
 
-def insert(self, key, value):
-    root = self.root
+            self.insert_non_full(node.children[i], key, value)
 
-    if len(root.keys) == ORDER - 1:
-        new_root = BTreeNode(leaf=False)
-        new_root.children.append(root)
+    def insert(self, key, value):
+        root = self.root
 
-        self.split_child(new_root, 0)
-        self.root = new_root
+        if len(root.keys) == ORDER - 1:
+            new_root = BTreeNode(leaf=False)
+            new_root.children.append(root)
 
-    self.insert_non_full(self.root, key, value)
+            self.split_child(new_root, 0)
+            self.root = new_root
+
+        self.insert_non_full(self.root, key, value)
