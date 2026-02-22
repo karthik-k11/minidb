3230a75543df34cb6a10c9726e3df8f9a112b070
Karthik <karthikk1162@gmail.com>
2026-02-22 21:44:59 +0530

    Verifying BTree

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from cli import start_cli
 from minidb.pager import Pager
+from minidb.btree import BTree
+
 
 pager = Pager()
 page_id = pager.allocate_page()
@@ -11,6 +13,14 @@ pager.write_page(page)
 
 page2 = pager.read_page(page_id)
 print(page2.read(0, 9))
+
+tree = BTree()
+
+for i in [10, 20, 5, 6, 12, 30, 7, 17]:
+    tree.insert(i, str(i))
+
+print(tree.search(tree.root, 6))
+print(tree.search(tree.root, 17))
 
 if __name__ == "__main__":
     start_cli()
