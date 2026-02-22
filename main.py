20deb528924cc1be24b6f700fb333d8706550ef5
Karthik <karthikk1162@gmail.com>
2026-02-22 21:23:36 +0530

    Imported pager to verify it

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from cli import start_cli
+from minidb.pager import Pager
+
+pager = Pager()
+page_id = pager.allocate_page()
+
+page = pager.read_page(page_id)
+page.write(0, b"HelloPage")
+
+pager.write_page(page)
+
+page2 = pager.read_page(page_id)
+print(page2.read(0, 9))
 
 if __name__ == "__main__":
     start_cli()
