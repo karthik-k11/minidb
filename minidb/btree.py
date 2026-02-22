4cf7799511c11eb76af2f3e52d5f99eb37156ba2
Karthik <karthikk1162@gmail.com>
2026-02-22 21:30:18 +0530

    Defined BTree skeleton

--- minidb/btree.py
+++ minidb/btree.py
@@ -6,3 +6,20 @@ class BTreeNode:
         self.keys = []
         self.values = []
         self.children = []
+
+class BTree:
+    def __init__(self):
+        self.root = BTreeNode()
+
+    def search(self, node, key):
+        i = 0
+        while i < len(node.keys) and key > node.keys[i]:
+            i += 1
+
+        if i < len(node.keys) and key == node.keys[i]:
+            return node.values[i]
+
+        if node.leaf:
+            return None
+
+        return self.search(node.children[i], key)
