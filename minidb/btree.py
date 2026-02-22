27148a3eb385ae4e24ee6cde0e4b92bd86642c34
Karthik <karthikk1162@gmail.com>
2026-02-22 21:42:38 +0530

    Function for splitting root

--- minidb/btree.py
+++ minidb/btree.py
@@ -23,3 +23,22 @@ class BTree:
             return None
 
         return self.search(node.children[i], key)
+
+def split_child(self, parent, index):
+    full_child = parent.children[index]
+    new_child = BTreeNode(leaf=full_child.leaf)
+    mid = ORDER // 2
+
+    parent.keys.insert(index, full_child.keys[mid])
+    parent.values.insert(index, full_child.values[mid])
+    parent.children.insert(index + 1, new_child)
+
+    new_child.keys = full_child.keys[mid + 1:]
+    new_child.values = full_child.values[mid + 1:]
+
+    full_child.keys = full_child.keys[:mid]
+    full_child.values = full_child.values[:mid]
+
+    if not full_child.leaf:
+        new_child.children = full_child.children[mid + 1:]
+        full_child.children = full_child.children[:mid + 1]
