3caea0f90605473e45306710d744794812480db9
Karthik <karthikk1162@gmail.com>
2026-02-27 14:22:01 +0530

    Query passing setup

---
+++ minidb/query.py
@@ -0,0 +1,37 @@
+class CreateTable:
+    def __init__(self, table_name):
+        self.table_name = table_name
+
+
+class Insert:
+    def __init__(self, table_name, key, value):
+        self.table_name = table_name
+        self.key = int(key)
+        self.value = int(value)
+
+
+class Select:
+    def __init__(self, table_name, key):
+        self.table_name = table_name
+        self.key = int(key)
+
+
+def parse(command: str):
+    parts = command.strip().split()
+
+    if not parts:
+        return None
+
+    cmd = parts[0].upper()
+
+    if cmd == "CREATE" and parts[1].upper() == "TABLE":
+        return CreateTable(parts[2])
+
+    elif cmd == "INSERT":
+        return Insert(parts[1], parts[2], parts[3])
+
+    elif cmd == "SELECT":
+        return Select(parts[1], parts[2])
+
+    else:
+        raise Exception("Invalid command")
