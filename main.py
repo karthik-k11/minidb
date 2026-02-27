86b1204b93a2a147b51502cec1968d31394f6d0a
Karthik <karthikk1162@gmail.com>
2026-02-27 14:25:40 +0530

    Only for CLI testing

--- main.py
+++ main.py
@@ -1,47 +1,4 @@
 from cli import start_cli
-from minidb.pager import Pager
-from minidb.btree import BTree
-from minidb.disk_btree import DiskBTree
-from minidb.db_engine import MiniDB
-
-db = MiniDB()
-
-db.create_table("users")
-users = db.get_table("users")
-
-for i in [1, 2, 3, 4, 5]:
-    users.insert(i, i * 100)
-
-print(users.select(3))
-print(users.select(5))
-
-pager = Pager()
-page_id = pager.allocate_page()
-
-page = pager.read_page(page_id)
-page.write(0, b"HelloPage")
-
-pager.write_page(page)
-
-page2 = pager.read_page(page_id)
-print(page2.read(0, 9))
-
-tree = BTree()
-
-for i in [10, 20, 5, 6, 12, 30, 7, 17]:
-    tree.insert(i, str(i))
-
-print(tree.search(tree.root, 6))
-print(tree.search(tree.root, 17))
-
-tree = DiskBTree()
-
-for i in [10, 20, 5, 6, 12, 30, 7, 17, 3, 4, 8]:
-    tree.insert(i, i * 10)
-
-print(tree.search(tree.root_page, 3))
-print(tree.search(tree.root_page, 17))
-print(tree.search(tree.root_page, 30))
 
 if __name__ == "__main__":
     start_cli()
